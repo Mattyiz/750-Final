f8d2cdd5b735307b621d5dc78aa4ee89d7f64328
Matt Izzo <46375091+Mattyiz@users.noreply.github.com>
2024-05-05 19:41:06 -0400

    Created different AI for the final level

--- Assets/Scripts/Shadow.cs
+++ Assets/Scripts/Shadow.cs
@@ -8,6 +8,10 @@ public class Shadow : MonoBehaviour
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private float speed = .15f;
     [SerializeField] private float cooldownTime = .5f;
+    [SerializeField] private bool finalLevel;
+    private int aiPhase;
+    private Vector3 targetPoint;
+    private int direction;
 
 
     private Vector3 playerPosition;
@@ -28,6 +32,10 @@ public class Shadow : MonoBehaviour
         showCollision = false;
         line = this.GetComponent<LineRenderer>();
 
+        aiPhase = 0;
+        targetPoint = transform.position;
+        direction = -1;
+
     }
 
     // Update is called once per frame
@@ -37,6 +45,13 @@ public class Shadow : MonoBehaviour
 
         if (!playerMovement.menuUp)
         {
+
+            if(finalLevel)
+            {
+                FinalAI();
+                return;
+            }
+
             positionCooldown -= Time.fixedDeltaTime;
             if (positionCooldown <= 0)
             {
@@ -122,4 +137,86 @@ public class Shadow : MonoBehaviour
             showCollision = true;
         }
     }
+
+    public void FinalAI()
+    {
+
+        if(aiPhase <= 0)
+        {
+            aiPhase = Random.Range(4, 5);
+        }
+
+        Vector3 distance;
+
+        switch (aiPhase)
+        {
+
+            case 1:
+                //Normal behavior
+                positionCooldown -= Time.fixedDeltaTime;
+                if (positionCooldown <= 0)
+                {
+                    oldPlayerPosition = playerPosition;
+                    playerPosition = player.transform.position;
+                    positionCooldown = cooldownTime;
+                }
+
+                distance = oldPlayerPosition - transform.position;
+
+                distance.Normalize();
+                distance = distance * speed;
+                transform.position += distance;
+                break;
+            case 2:
+                //Go to random position
+                if ((targetPoint.x + speed >= transform.position.x && targetPoint.y + speed >= transform.position.y) ||
+                    (targetPoint.x - speed <= transform.position.x && targetPoint.y - speed <= transform.position.y))
+                {
+                    targetPoint = new Vector3(Random.Range(-35.0f, 35.0f), Random.Range(-20.0f, 20.0f), 0);
+                }
+
+                distance = targetPoint - transform.position;
+
+                distance.Normalize();
+                distance = distance * speed;
+                transform.position += distance;
+                break;
+            case 3:
+                //Goes from one side of the screen to the other before changing y direction. Inspired by the Centipede in the Atari game Centipede
+                if (targetPoint == transform.position)
+                {
+                    targetPoint = new Vector3(40, transform.position.y, 0);
+                }
+
+                if (transform.position.x >= 35 || transform.position.x <= -35)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y + (transform.localScale.y * direction), 0);
+                    targetPoint = new Vector3(targetPoint.x * -1, transform.position.y, 0);
+
+                    if(transform.position.y >= 19 || transform.position.y <= -15)
+                    {
+                        direction = direction * -1;
+                    }
+                }
+
+                distance = targetPoint - transform.position;
+
+                distance.Normalize();
+                distance = distance * speed;
+                transform.position += distance;
+
+                break;
+            case 4:
+
+                
+
+
+                break;
+            default:
+                aiPhase = 0;
+                break;
+        }
+
+    }
+
 }
